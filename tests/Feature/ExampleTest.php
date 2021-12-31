38010ceb2e68a9075b2049a6b24252ad1c27c483
TSuwa16 <tk.suwa16@gmail.com>
2021-12-31 16:23:09 +0900

    ユニットテスト作成

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,18 +4,23 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\Task;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /**
+     * @test
+     */
+    public function 一覧を取得()
+    {
+        $tasks = Task::factory()->count(10)->create();
+
+        $response = $this->getJson('api/tasks');
+
+        $response
+            ->assertOk()
+            ->assertJsonCount($tasks->count());
     }
 }
