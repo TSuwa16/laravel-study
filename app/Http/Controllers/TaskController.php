0f1f42bf08b6ba5768cff120ff20bcaacdf6efe8
TSuwa16 <tk.suwa16@gmail.com>
2021-12-31 15:51:00 +0900

    タスク一覧取得API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -9,24 +9,15 @@ use App\Http\Requests\UpdateTaskRequest;
 class TaskController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * タスクの一覧を取得
      *
-     * @return \Illuminate\Http\Response
+     * @return Task::all
      */
     public function index()
     {
-        //
+        return Task::orderByDesc('id')->get();
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -46,17 +37,6 @@ class TaskController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function show(Task $task)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Task $task)
     {
         //
     }
